3792fce24791f83f885d316f24884dceafd3c535
Chase <Chase@10.0.0.12>
2014-06-10 15:27:13 -0600

    updates

--- src/android/FileChecksum.java
+++ src/android/FileChecksum.java
@@ -1,4 +1,4 @@
-package com.chasenoel.cordova.filechecksum;
+package com.chasenoel.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -9,6 +9,9 @@ import org.json.JSONObject;
 
 import java.io.FileInputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileChecksum extends CordovaPlugin {
     
@@ -33,7 +36,13 @@ public class FileChecksum extends CordovaPlugin {
     }
     
     private void getChecksum(String filename, CallbackContext callbackContext){
-        
+        try{
+            String hash = sha1Checksum(filename);
+            callbackContext.success(hash);
+        }
+        catch(Exception e){
+            callbackContext.error(e.getMessage());
+        }
     }
     
     private void getChecksums(JSONArray files, CallbackContext callbackContext){
@@ -42,5 +51,27 @@ public class FileChecksum extends CordovaPlugin {
     
     private void compareFileToChecksum(JSONObject filesAndChecksums, CallbackContext callbackContext){
         
+    }
+    
+    private String sha1Checksum(String file) throws FileNotFoundException,NoSuchAlgorithmException,IOException{
+        
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        FileInputStream fis = new FileInputStream(file);
+        byte[] dataBytes = new byte[1024];
+
+        int nread = 0; 
+
+        while ((nread = fis.read(dataBytes)) != -1) {
+          md.update(dataBytes, 0, nread);
+        };
+
+        byte[] mdbytes = md.digest();
+
+        //convert the byte to hex format
+        StringBuffer sb = new StringBuffer("");
+        for (int i = 0; i < mdbytes.length; i++) {
+            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
     }
 }
--- www/file-checksum.js
+++ www/file-checksum.js
@@ -1,11 +1,47 @@
-var FileChecksum = {
-    getChecksum: function(file){
-        
-    },
-    getChecksums: function(files){
-        
-    },
-    compareFileToChecksum: function(filesWithChecksum){
-        
-    }
+var exec = require('cordova/exec');
+
+var FileChecksum = function(){};
+
+FileChecksum.prototype.getChecksum = function(file){
+    exec(
+        function successCallback(resp){
+            return resp;
+        },
+        function errorCallback(err){
+            return err;
+        },
+        'FileChecksum',
+        'getChecksum',
+        [file]
+    );
 };
+
+FileChecksum.prototype.getChecksums = function(files){
+    exec(
+        function successCallback(resp){
+            return resp;
+        },
+        function errorCallback(err){
+            return err;
+        },
+        'FileChecksum',
+        'getChecksums',
+        [files]
+    );
+};
+
+FileChecksum.prototype.compareFileToChecksum = function(filesWithChecksum){
+    exec(
+        function successCallback(resp){
+            return resp;
+        },
+        function errorCallback(err){
+            return err;
+        },
+        'FileChecksum',
+        'compareFileToChecksum',
+        [filesWithChecksum]
+    );
+};
+
+module.exports = FileChecksum;
